918a505ea067d445b588b7e7565030bb643e8a1c
Vladislav.Klochkov <Vladislav.Klochkov@billing.ru>
2018-04-05 21:47:19 +0300

    update_05.04.2018

---
+++ src/main/java/com/uraltranscom/service/GetCountryCodeOfStation.java
@@ -0,0 +1,21 @@
+package com.uraltranscom.service;
+
+/**
+ *
+ * Интерфейс проверки корректности кода станции
+ *
+ * @author Vladislav Klochkov
+ * @version 4.1
+ * @create 12.01.2018
+ *
+ * 12.01.2018
+ *   1. Версия 3.0
+ * 14.03.2018
+ *   1. Версия 4.0
+ * 03.04.2018
+ *   1. Версия 4.1
+ *
+ */
+
+public interface GetCountryCodeOfStation {
+}
